3d0ab34cf19184b69d1c3bea0cb5b1c1aae6966f
Nuria Perez Casas <55450564+nperezcasas@users.noreply.github.com>
2021-10-12 23:22:04 -0500

    Add files via upload

---
+++ EventConsumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace EventConsumer
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                using(TcpClient client = new TcpClient("127.0.0.1", 12500))
+                {
+                    using (NetworkStream stream = client.GetStream())
+                        while (true)
+                        {
+                            try
+                            {
+                                byte[] dataReceived = new byte[256];
+                                Int32 bytes = stream.Read(dataReceived, 0, dataReceived.Length);
+                                var responseData = System.Text.Encoding.ASCII.GetString(dataReceived, 0, bytes);
+                                Console.WriteLine(responseData);
+                            }
+                            catch
+                            {
+                                break;
+                            }
+                        }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n\nPress Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
